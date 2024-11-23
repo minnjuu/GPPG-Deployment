f9958448d72eef4153af46d9b00b4b23b2860eea
Nick Francis Bundal <nnikeetmg@gmail.com>
2024-11-23 14:29:05 +0800

    test map config

--- GPPG/GPPG/settings.py
+++ GPPG/GPPG/settings.py
@@ -205,13 +205,19 @@ AWS_S3_FILE_OVERWRITE = False
 
 STORAGES = {
     "default": {
-        "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        "OPTIONS": {
+            "location": "media/",  
+        },
     },
     "staticfiles": {
-        "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        "OPTIONS": {
+            "location": "static/",  
+        },
     },
 }
-STATIC_URL = 'https://gppg-bucket.s3.amazonaws.com/static/'
+STATIC_URL = "/static/"
 
 
 STATICFILES_DIRS = [
